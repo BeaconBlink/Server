2156ba4df3d89c0da2a004e7368706c8a7f6b2e4
Dawid Kardacz <dkardacz561@gmail.com>
2024-06-16 19:37:56 +0200

    port fix

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import {connect} from "./db";
 import {Db} from "mongodb";
 
 const app = express();
-const PORT = 8082;
+const PORT = 8080;
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "../public")));
